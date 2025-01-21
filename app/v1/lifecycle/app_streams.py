c67299e6a01892b8dc434f45c341cd39072f6582
Sam Doran <sdoran@redhat.com>
2025-01-20 21:42:32 -0500

    Change format of app stream lifecycle data
    Make responses a bit more consistent by always returning a dictionary.

--- app/v1/lifecycle/app_streams.py
+++ app/v1/lifecycle/app_streams.py
@@ -26,21 +26,21 @@ async def get_app_streams(
 
         return {"data": result}
 
-    return {"data": [module for module in MODULE_DATA.values()]}
+    return {"data": [module for module in MODULE_DATA]}
 
 
 @v1_router.get("/{major_version}")
 async def get_major_version(
     major_version: t.Annotated[int, Path(description="Major RHEL version", gt=1, le=200)],
 ):
-    return {"data": MODULE_DATA.get(major_version)}
+    return {"data": [module for module in MODULE_DATA if module.get("rhel_major_version", 0) == major_version]}
 
 
 @v1_router.get("/{major_version}/names")
 async def get_module_names(
     major_version: t.Annotated[int, Path(description="Major RHEL version", gt=1, le=200)],
 ) -> dict[str, list[str]]:
-    data = MODULE_DATA.get(major_version, [])
+    data = [module for module in MODULE_DATA if module.get("rhel_major_version", 0) == major_version]
     return {"names": sorted(item["module_name"] for item in data)}
 
 
@@ -49,9 +49,9 @@ async def get_module(
     major_version: t.Annotated[int, Path(description="Major RHEL version", gt=1, le=200)],
     module_name: t.Annotated[str, Path(description="Module name")],
 ):
-    if data := MODULE_DATA.get(major_version):
+    if data := [module for module in MODULE_DATA if module.get("rhel_major_version", 0) == major_version]:
         if modules := sorted(item for item in data if item.get("module_name") == module_name):
-            return modules
+            return {"data": modules}
 
     return JSONResponse(
         content={"message": "No modules matches query", "query": module_name},

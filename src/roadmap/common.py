d8e9ee932bd8169927ab053ae455ac7287443425
Sam Doran <sdoran@redhat.com>
2025-02-10 17:59:52 -0500

    Get system counts from inventory
    Only attempt when an authorization header exists.

--- src/roadmap/common.py
+++ src/roadmap/common.py
@@ -1,24 +1,40 @@
+import json
 import typing as t
 import urllib.parse
 import urllib.request
 
+from urllib.error import HTTPError
 
-async def get_all_systems(headers: dict[t.Any, t.Any]):
+from starlette.datastructures import Headers
+
+
+# FIXME: This should be cached
+async def get_system_count_from_inventory(headers: Headers) -> dict[str, t.Any]:
+    # When Host has "localhost" in it, an error is returned.
+    # Only include the headers that are necessary.
+    # This could also change to excluding problematic headers.
+    include = ["user-agent", "authorization"]
+    hdrs = {k: v for k, v in headers.items() if k in include}
+
+    if "authorization" not in hdrs:
+        # If we don't have a token, do not try to query the API.
+        # This could be a dev/test environment.
+        return {}
+
     params = {
-        "per_page": 5,
+        "per_page": 100,
         "page": 1,
-        # "staleness": ["fresh", "stale", "stale_warning"],
+        "staleness": ["fresh", "stale", "stale_warning"],
         "order_by": "operating_system",
     }
     req = urllib.request.Request(
-        # f"https://console.redhat.com/api/inventory/v1/system_profile/operating_system?{urllib.parse.urlencode(params)}",
-        f"https://httpbin.org/get?{urllib.parse.urlencode(params)}",
-        headers=headers,
+        f"https://console.redhat.com/api/inventory/v1/system_profile/operating_system?{urllib.parse.urlencode(params, doseq=True)}",
+        headers=hdrs,
     )
     try:
         with urllib.request.urlopen(req) as response:
-            data = response.read()
-    except urllib.error.HTTPError as err:
+            data = json.load(response)
+    except HTTPError as err:
         return {"error": str(err)}
 
     return data
--- src/roadmap/v1/lifecycle/rhel.py
+++ src/roadmap/v1/lifecycle/rhel.py
@@ -3,13 +3,11 @@ import typing as t
 from operator import attrgetter
 
 from fastapi import APIRouter
-from fastapi import Header
-from fastapi import Response
 from fastapi import Path
 from fastapi import Request
 
-from roadmap.common import get_all_systems
-from roadmap.data.systems import OS_DATA_MOCKED
+from roadmap.common import get_system_count_from_inventory
+from roadmap.data.systems import OS_LIFECYCLE_DATES
 from roadmap.models import System
 
 
@@ -20,18 +18,24 @@ router = APIRouter(
 
 
 @router.get("/")
-async def get_systems(request: Request):
-    inventory_systems = await get_all_systems(request.headers)
-    return Response(inventory_systems)
-
+async def get_systems(request: Request) -> list[System]:
+    systems_response = await get_system_count_from_inventory(request.headers)
     systems = get_systems_data()
+
+    for system in systems:
+        system.count = 0
+        for item in systems_response.get("results", []):
+            value = item["value"]
+            if (system.name, system.major, system.minor) == (value["name"], value["major"], value["minor"]):
+                system.count = item["count"]
+                break
 
     return sorted(systems, key=attrgetter("major", "minor"), reverse=True)
 
 
 @router.get("/{major}")
-    systems = get_systems_data(major)
 async def get_systems_major(major: t.Annotated[int, Path(description="Major version number")]) -> list[System]:
+    systems = get_systems_data(major)
 
     return sorted(systems, key=attrgetter("major", "minor"), reverse=True)
 
@@ -47,11 +51,23 @@ async def get_systems_major_minor(
 
 
 def get_systems_data(major=None, minor=None):
-    data = OS_DATA_MOCKED
+    data = [
+        System(
+            name=item.name,
+            major=item.major,
+            minor=getattr(item, "minor", None) or 0,
+            release="release",
+            release_date=item.start,
+            retirement_date=item.end,
+            lifecycle_type="lifecycle type",
+        )
+        for item in OS_LIFECYCLE_DATES.values()
+        if getattr(item, "minor") is not None
+    ]
 
     if major is not None:
-        data = [d for d in data if d.major == major]
+        data = [item for item in data if item.major == major]
     if minor is not None:
-        data = [d for d in data if d.minor == minor]
+        data = [item for item in data if item.minor == minor]
 
     return data

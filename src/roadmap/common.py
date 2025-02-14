8f2c9fb1afcc69b56378405694bf4c49523f26de
Sam Doran <sdoran@redhat.com>
2025-02-14 11:17:29 -0500

    Add ability to return fixture data
    This will be helpful for dev/testing purposes.

--- src/roadmap/common.py
+++ src/roadmap/common.py
@@ -7,7 +7,9 @@ import urllib.request
 from pathlib import Path
 from urllib.error import HTTPError
 
-import roadmap.config as C
+from fastapi import HTTPException
+
+from roadmap.config import SETTINGS
 
 
 logger = logging.getLogger("uvicorn.error")
@@ -15,16 +17,23 @@ logger = logging.getLogger("uvicorn.error")
 
 # FIXME: This should be cached
 async def get_system_count_from_inventory(headers: dict[str, str | None]) -> dict[str, t.Any]:
+    if SETTINGS.dev:
+        logger.debug("Running in development mode. Returning fixture response data for inventory.")
+        file = Path(__file__).resolve()
+        response_data = file.parent.parent.parent / "tests" / "fixtures" / "inventory_response.json"
+        return json.loads(response_data.read_text())
+
     if not headers.get("Authorization"):
         # If we don't have a token, do not try to query the API.
         # This could be a dev/test environment.
+        logger.info("Missing authorization header. Unable to get inventory.")
         return {}
 
     # Filter out missing header values
     headers = {k: v for k, v in headers.items() if v is not None}
     params = {
         "per_page": 100,
-        "page": 1,
+        "page": 100,
         "staleness": ["fresh", "stale", "stale_warning"],
         "order_by": "updated",
         "fields[system_profile]": ",".join(
@@ -48,6 +57,6 @@ async def get_system_count_from_inventory(headers: dict[str, str | None]) -> dic
             data = json.load(response)
     except HTTPError as err:
         logger.error(f"Problem getting systems from inventory: {err}")
-        return {"error": str(err)}
+        raise HTTPException(status_code=err.code, detail=err.msg)
 
     return data

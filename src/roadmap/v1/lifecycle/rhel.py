8015b93ee2549a808b6c6d42d34c8a8634d13f54
Sam Doran <sdoran@redhat.com>
2025-02-14 11:14:28 -0500

    Use path from router and cleanup tags

--- src/roadmap/v1/lifecycle/rhel.py
+++ src/roadmap/v1/lifecycle/rhel.py
@@ -19,8 +19,6 @@ router = APIRouter(
 MajorVersion = t.Annotated[int, Path(description="Major version number", ge=8, le=10)]
 MinorVersion = t.Annotated[int, Path(description="Minor version number", ge=0, le=10)]
 
-@router.get("/")
-async def get_systems(request: Request) -> list[System]:
     systems_response = await get_system_count_from_inventory(request.headers)
     systems = get_systems_data()
 
@@ -31,29 +29,37 @@ async def get_systems(request: Request) -> list[System]:
             if (system.name, system.major, system.minor) == (value["name"], value["major"], value["minor"]):
                 system.count = item["count"]
                 break
+@router.get("/", summary="Return lifecycle data for all RHEL versions")
+async def get_systems() -> list[RHELLifecycle]:
+@router.get("/{major}")
+async def get_systems_major(
+    major: MajorVersion,
+) -> list[RHELLifecycle]:
+@router.get("/{major}/{minor}")
+async def get_systems_major_minor(
 
     return sorted(systems, key=attrgetter("major", "minor"), reverse=True)
 ## Relevant ##
 relevant = APIRouter(
-    prefix="/relevant/systems",
-    tags=["relevant", "rhel", "systems"],
+    prefix="/relevant/rhel",
+    tags=["Relevant", "RHEL"],
 )
 
 
-@relevant.get("/rhel")
+@relevant.get("/")
 async def get_relevant_systems(
     authorization: t.Annotated[str | None, Header(include_in_schema=False)] = None,
     user_agent: t.Annotated[str | None, Header(include_in_schema=False)] = None,
     x_rh_identity: t.Annotated[str | None, Header(include_in_schema=False)] = None,
 ) -> dict[str, list[System] | str | int]:
-@relevant.get("/rhel/{major}")
+@relevant.get("/{major}")
 async def get_relevant_systems_major(major: t.Annotated[int, MajorVersion]) -> list[System]:
     systems = get_systems_data(major)
 
     return sorted(systems, key=attrgetter("major", "minor"), reverse=True)
 
 
-@relevant.get("/rhel/{major}/{minor}")
+@relevant.get("/{major}/{minor}")
 async def get_relevant_systems_major_minor(
     major: MajorVersion,
     minor: MinorVersion,

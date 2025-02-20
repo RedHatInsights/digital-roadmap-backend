7e7d9c242d26b275aadafb97f7882faf94ceecc0
Sam Doran <sdoran@redhat.com>
2025-02-20 17:19:40 -0500

    Add tests for major/minor

--- tests/test_common.py
+++ tests/test_common.py
@@ -21,6 +21,49 @@ async def test_get_system_count_from_inventory(mocker, read_fixture_file):
     assert response["count"] == 100
 
 
+async def test_get_system_count_from_inventory_major(mocker, read_fixture_file):
+    mocker.patch(
+        "roadmap.common.urllib.request.urlopen",
+        return_value=BytesIO(read_fixture_file("inventory_response_9.json", mode="rb")),
+    )
+    headers: dict[str, str | None] = {"Authorization": "Bearer token"}
+    response = await get_system_count_from_inventory(headers, major=9)
+    versions = {item["system_profile"]["operating_system"]["major"] for item in response["results"]}
+
+    assert len(response["results"]) > 1
+    assert versions == {9}
+
+
+async def test_get_system_count_from_inventory_major_minor(mocker, read_fixture_file):
+    mocker.patch(
+        "roadmap.common.urllib.request.urlopen",
+        return_value=BytesIO(read_fixture_file("inventory_response_9.5.json", mode="rb")),
+    )
+    headers: dict[str, str | None] = {"Authorization": "Bearer token"}
+    response = await get_system_count_from_inventory(headers, major=9, minor=5)
+    major_versions = {item["system_profile"]["operating_system"]["major"] for item in response["results"]}
+    minor_versions = {item["system_profile"]["operating_system"]["minor"] for item in response["results"]}
+
+    assert len(response["results"]) > 1
+    assert major_versions == {9}, "Major version mismatch"
+    assert minor_versions == {5}, "Minor version mismatch"
+
+
+async def test_get_system_count_from_inventory_major_minor_zero(mocker, read_fixture_file):
+    mocker.patch(
+        "roadmap.common.urllib.request.urlopen",
+        return_value=BytesIO(read_fixture_file("inventory_response_9.0.json", mode="rb")),
+    )
+    headers: dict[str, str | None] = {"Authorization": "Bearer token"}
+    response = await get_system_count_from_inventory(headers, major=9, minor=0)
+    major_versions = {item["system_profile"]["operating_system"]["major"] for item in response["results"]}
+    minor_versions = {item["system_profile"]["operating_system"]["minor"] for item in response["results"]}
+
+    assert len(response["results"]) > 1
+    assert major_versions == {9}, "Major version mismatch"
+    assert minor_versions == {0}, "Minor version mismatch"
+
+
 async def test_get_system_count_from_inventory_missing_auth():
     result = await get_system_count_from_inventory({})
 

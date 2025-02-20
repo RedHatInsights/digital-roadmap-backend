066b6c077057d2e804c8926e8d0e4e1bb2ab904e
Sam Doran <sdoran@redhat.com>
2025-02-20 16:17:18 -0500

    Add fixture for reading file contents

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,3 +22,16 @@ def read_json_fixture():
         return json.loads(fixture_path.joinpath(file).read_text())
 
     return _read_json_file
+
+
+@pytest.fixture
+def read_fixture_file():
+    fixture_path = Path(__file__).parent.joinpath("fixtures").resolve()
+
+    def _read_file(file: Path, mode):
+        with fixture_path.joinpath(file).open(mode) as f:
+            data = f.read()
+
+        return data
+
+    return _read_file

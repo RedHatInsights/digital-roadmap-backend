c3c3416e99de9be87dfa0a65401ab89aa9cbccea
Sam Doran <sdoran@redhat.com>
2025-04-10 16:19:54 -0400

    Query database directly to get inventory data
    Co-authored-by: Chris Duryee <cduryee@redhat.com>

--- src/roadmap/common.py
+++ src/roadmap/common.py
@@ -1,15 +1,14 @@
+import base64
 import gzip
 import json
 import logging
 import typing as t
-import urllib.parse
-import urllib.request
 
 from datetime import date
 from pathlib import Path
-from urllib.error import HTTPError
 
-from fastapi import HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql import text
 
 from roadmap.config import SETTINGS
 from roadmap.models import LifecycleType
@@ -25,13 +24,15 @@ class HealthCheckFilter(logging.Filter):
 
 # FIXME: This should be cached
 async def query_host_inventory(
-    headers: dict[str, str | None],
-    page: int = 1,
-    per_page: int = 100,
+    session: AsyncSession,
+    org_id: str,
     major: int | None = None,
     minor: int | None = None,
-) -> dict[str, t.Any]:
+):
     if SETTINGS.dev:
+        if not org_id:
+            org_id = "test"
+
         logger.debug("Running in development mode. Returning fixture response data for inventory.")
         file = Path(__file__).resolve()
         logger.debug(f"{major=} {minor=}")
@@ -56,50 +57,10 @@ async def query_host_inventory(
 
         return response_data
 
-    if not any(headers.get(value) for value in ("Authorization", "X-RH-Identity")):
-        # If we don't have a token, do not try to query the API.
-        # This could be a dev/test environment.
-        logger.info("Missing authorization header. Unable to get inventory.")
-        return {}
-
-    # Filter out missing header values
-    headers = {k: v for k, v in headers.items() if v is not None}
-    params = {
-        "per_page": per_page,
-        "page": page,
-        "staleness": ["fresh", "stale", "stale_warning"],
-        "order_by": "updated",
-        "fields[system_profile]": ",".join(
-            # TODO: Make these fields a parameter
-            [
-                "dnf_modules",
-                "operating_system",
-                "rhsm",
-                "installed_packages",
-                "installed_products",
-            ]
-        ),
-    }
-    if any(value is not None for value in (major, minor)):
-        # Build the filter value of either "{major}" or "{major}.{minor}",
-        # such as "9", or "9.5".
-        params["filter[system_profile][operating_system][RHEL][version][eq]"] = (
-            f"{major}{'.' + str(minor) if minor is not None else ''}"
-        )
-
-    req = urllib.request.Request(
-        f"{SETTINGS.host_inventory_url}/api/inventory/v1/hosts?{urllib.parse.urlencode(params, doseq=True)}",
-        headers=headers,  # pyright: ignore [reportArgumentType]
+    result_set = await session.execute(
+        text("SELECT * FROM hbi.hosts WHERE org_id = :org_id;"), params={"org_id": org_id}
     )
-
-    try:
-        with urllib.request.urlopen(req) as response:
-            data = json.load(response)
-    except HTTPError as err:
-        logger.error(f"Problem getting systems from inventory: {err}")
-        raise HTTPException(status_code=err.code, detail=err.msg)
-
-    return data
+    return result_set.mappings().all()
 
 
 def get_lifecycle_type(products: list[dict[str, str]]) -> LifecycleType:

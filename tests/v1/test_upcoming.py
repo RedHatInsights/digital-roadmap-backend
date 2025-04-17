f5b42ed7e33ab4cc4b655f88efd828e49e1ebe11
Ellis Low <elow@redhat.com>
2025-04-17 13:08:52 -0400

    test upcoming json env var

--- tests/v1/test_upcoming.py
+++ tests/v1/test_upcoming.py
@@ -1,3 +1,20 @@
-def test_get_upcoming_changes(client, api_prefix):
+import importlib
+
+from pathlib import Path
+
+import roadmap.v1.upcoming
+
+
+def test_get_upcoming_changes(client, api_prefix, monkeypatch):
     response = client.get(f"{api_prefix}/upcoming-changes")
     assert response.status_code == 200
+    assert response.content.startswith(b'[{"name":"Node.js 22 included in RHEL 9 Application Streams","type":')
+
+    monkeypatch.setenv(
+        "ROADMAP_UPCOMING_JSON_PATH",
+        str(Path(__file__).parent.parent.joinpath("fixtures").resolve().joinpath("upcoming.json")),
+    )
+    importlib.reload(roadmap.v1.upcoming)
+    response = client.get(f"{api_prefix}/upcoming-changes")
+    assert response.status_code == 200
+    assert response.content.startswith(b'[{"name":"Node.js 22 included in RHEL 9 Application Streams TEST","type":')

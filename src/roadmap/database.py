28cffc72089c7e8759e277a381ed1e375ac0f3fe
Sam Doran <sdoran@redhat.com>
2025-04-21 17:25:05 -0400

    Use dependency injection for determining access and querying database
    This required moving from streaming results from the DB query to processing them
    all and returning the result. This is due to how FastAPI resolves dependencies
    internally.
    
    It’s possible there is a way to use streaming results from the database with
    dependency injection, but I do not currently know how.

--- src/roadmap/database.py
+++ src/roadmap/database.py
@@ -1,10 +1,13 @@
+import typing as t
+
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine
 
 from roadmap.config import Settings
 
 
-async def get_db(settings: Settings):
+async def get_db(settings: t.Annotated[Settings, Depends(Settings.create)]):
     engine = create_async_engine(str(settings.database_url), echo=True)
     async_session = async_sessionmaker(engine, expire_on_commit=False)
     async with async_session() as session:

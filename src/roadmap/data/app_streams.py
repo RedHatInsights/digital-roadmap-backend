d13b29ed790352d32388b28a7696f3721150e194
Ellis Low <elow@redhat.com>
2025-04-24 12:38:34 -0400

    slight change to dict set creation

--- src/roadmap/data/app_streams.py
+++ src/roadmap/data/app_streams.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import typing as t
 
 from datetime import date
@@ -7768,13 +7769,9 @@ APP_STREAM_MODULES = [
 
 APP_STREAM_MODULES_BY_KEY = {(asm.name, asm.os_major, asm.stream): asm for asm in APP_STREAM_MODULES}
 
-OS_MAJORS_BY_APP_NAME = {}
+OS_MAJORS_BY_APP_NAME = defaultdict(set)
 for asm in APP_STREAM_MODULES:
-    try:
-        major_versions = OS_MAJORS_BY_APP_NAME[asm.name]
-    except KeyError:
-        major_versions = set()
-        OS_MAJORS_BY_APP_NAME[asm.name] = major_versions
-    major_versions.add(asm.os_major)
+    OS_MAJORS_BY_APP_NAME[asm.name].add(asm.os_major)
+OS_MAJORS_BY_APP_NAME = dict(OS_MAJORS_BY_APP_NAME)
 
 APP_STREAM_MODULES_PACKAGES = [*APP_STREAM_MODULES, *APP_STREAM_PACKAGES.values()]

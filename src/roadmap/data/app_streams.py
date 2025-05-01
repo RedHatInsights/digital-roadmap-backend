29a138f4ff2a0367aedfc3f0b66658ab18d26228
Ellis Low <elow@redhat.com>
2025-05-01 10:52:23 -0400

    optimize display_name

--- src/roadmap/data/app_streams.py
+++ src/roadmap/data/app_streams.py
@@ -17,6 +17,22 @@ from roadmap.data.systems import OS_LIFECYCLE_DATES
 
 
 Date = t.Annotated[str | date, AfterValidator(ensure_date)]
+_DISPLAY_NAME_SPECIAL_CASES = {
+    "apache httpd": "Apache HTTPD",
+    "freeradius": "FreeRADIUS",
+    "llvm": "LLVM",
+    "mariadb": "MariaDB",
+    "mod_auth_openidc for apache": "Mod Auth OpenIDC for Apache",
+    "mysql": "MySQL",
+    "nginx": "NGINX",
+    "node.js": "Node.js",
+    "nodejs": "Node.js",
+    "openjdk": "OpenJDK",
+    "osinfo-db": "OSInfo DB",
+    "php": "PHP",
+    "postgresql": "PostgreSQL",
+    "rhn-tools": "RHN Tools",
+}
 
 
 class AppStreamImplementation(StrEnum):
@@ -84,22 +100,6 @@ class AppStreamEntity(BaseModel):
     def set_display_name(self):
         """Create a normalized name field for presentation"""
 
-        special_case = {
-            "apache httpd": "Apache HTTPD",
-            "freeradius": "FreeRADIUS",
-            "llvm": "LLVM",
-            "mariadb": "MariaDB",
-            "mod_auth_openidc for apache": "Mod Auth OpenIDC for Apache",
-            "mysql": "MySQL",
-            "nginx": "NGINX",
-            "node.js": "Node.js",
-            "nodejs": "Node.js",
-            "openjdk": "OpenJDK",
-            "osinfo-db": "OSInfo DB",
-            "php": "PHP",
-            "postgresql": "PostgreSQL",
-            "rhn-tools": "RHN Tools",
-        }
         display_name = self.name
         if self.application_stream_name and self.application_stream_name != "Unknown":
             display_name = self.application_stream_name
@@ -113,8 +113,8 @@ class AppStreamEntity(BaseModel):
                 display_name = f"{display_name.rstrip()} {version}"
 
         # Correct capitalization
-        for name, cased_name in special_case.items():
-            lower_name = display_name.lower()
+        lower_name = display_name.lower()
+        for name, cased_name in _DISPLAY_NAME_SPECIAL_CASES.items():
             if name in lower_name:
                 display_name = lower_name.replace(name, cased_name)
                 break

912df7b6e889d21be851efc6bc0c8d33a7b39961
Sam Doran <sdoran@redhat.com>
2025-07-24 10:58:40 -0400

    Move variable calculation into a function
    Having a loop at the module global level is messy. Encapsulating the data
    transformation in a function in much cleaner.

--- src/roadmap/data/__init__.py
+++ src/roadmap/data/__init__.py
@@ -4,11 +4,14 @@ from .modules import APP_STREAM_MODULES
 from .packages import APP_STREAM_PACKAGES
 
 
+def _os_majors_by_app_name():
+    result = defaultdict(set)
+    for asm in APP_STREAM_MODULES:
+        result[asm.name].add(asm.os_major)
+
+    return dict(result)
+
+
 APP_STREAM_MODULES_BY_KEY = {(asm.name, asm.os_major, asm.stream): asm for asm in APP_STREAM_MODULES}
-
-OS_MAJORS_BY_APP_NAME = defaultdict(set)
-for asm in APP_STREAM_MODULES:
-    OS_MAJORS_BY_APP_NAME[asm.name].add(asm.os_major)
-OS_MAJORS_BY_APP_NAME = dict(OS_MAJORS_BY_APP_NAME)
-
+OS_MAJORS_BY_APP_NAME = _os_majors_by_app_name()
 APP_STREAM_MODULES_PACKAGES = [*APP_STREAM_MODULES, *APP_STREAM_PACKAGES.values()]

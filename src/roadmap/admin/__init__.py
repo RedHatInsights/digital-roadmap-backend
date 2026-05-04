7095375d17d1c29e9b677a85b98c198e7edd09c8
Sergio Santamaria <13274437+SerCantus@users.noreply.github.com>
2026-05-04 16:36:48 +0200

    Linting

--- src/roadmap/admin/__init__.py
+++ src/roadmap/admin/__init__.py
@@ -2,5 +2,6 @@ from fastapi import APIRouter
 
 from . import notificator
 
+
 router = APIRouter(prefix="/admin", tags=["Admin"], include_in_schema=False)
 router.include_router(notificator.router)

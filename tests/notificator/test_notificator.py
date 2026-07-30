52aab3d2c200e5471c64a823e531cf1f38db9173
Adam Hošek <ahosek@redhat.com>
2026-07-30 15:28:13 +0200

    Update config and yield early (#395)
    Current config with yield and allowing processing other requests like
    readinessProbe ad livenessProbe wasn't enough as processing 2000k hosts
    takes around 13 secs.
    
    Decreasing the amount of hosts to be processed before yield and setting
    timeout to have some headroom.
    
    Updating the memory allocation to current requirements - memory
    consumption is much lower now with streaming.

--- tests/notificator/test_notificator.py
+++ tests/notificator/test_notificator.py
@@ -728,9 +728,9 @@ class TestGetRelevantUpcoming:
 
         assert result == Counter({"addition": 1})
 
-    async def test_cooperative_yield_fires_every_2000_hosts(self, notificator, mocker):
+    async def test_cooperative_yield_fires_every_1000_hosts(self, notificator, mocker):
         """Verify asyncio.sleep(0) is called to yield control back to the event loop."""
-        hosts = [make_host_mapping(i, os_major=9) for i in range(1, 2_002)]
+        hosts = [make_host_mapping(i, os_major=9) for i in range(1, 2_001)]
         self._set_matching_scenario(
             [make_upcoming_input("addition", date(2026, 4, 10))],
             hosts=hosts,
@@ -739,7 +739,8 @@ class TestGetRelevantUpcoming:
 
         await notificator.get_relevant_upcoming()
 
-        mock_sleep.assert_called_once_with(0)
+        assert mock_sleep.call_count == 2
+        mock_sleep.assert_called_with(0)
 
     async def test_deployed_date_none_uses_today(self, notificator):
         """Items with deployedDate=None use today's date and pass the window check."""
